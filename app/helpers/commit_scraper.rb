6e40ba25e7edef7efe2153463c4cbe0d6a109a90
Jon Campbell <heycampbell@gmail.com>
2015-01-31 12:18:52 -0500

    add methods to pull all commits from a user

--- app/helpers/commit_scraper.rb
+++ app/helpers/commit_scraper.rb
@@ -1,19 +1,33 @@
 require 'open-uri'
 module GitScraper
 
-  def get_latest_push(username)
+  def get_all_pushes(username)
+    all_pushes = []
     events = JSON.parse(open("https://api.github.com/users/#{username}/events").read)
     events.each do |event|
       if event['type'] = 'PushEvent'
-        return event
+        all_pushes << event
       end
     end
+    all_pushes
+  end
+
+  def get_all_commits(username)
+    get_all_pushes(username).each do |push|
+      push['payload']['commits'].each do |commit|
+        commits << JSON.parse(open(commit['url']).read)
+      end
+    end
+    commits
+  end
+
+  def get_latest_push(username)
+    get_all_pushes(username).first
   end
 
   def most_recent_commit(username)
-    latest = get_latest_push(username)
     recent_commits = []
-    latest['payload']['commits'].each do |commit|
+    get_latest_push(username)['payload']['commits'].each do |commit|
       recent_commits << JSON.parse(open(commit['url']).read)
     end
     recent_commits
