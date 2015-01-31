f7fa7f209ee978aa59367791deea92d2fe335e41
Dylan Shine <dylanshinenyc@gmail.com>
2015-01-31 12:57:21 -0500

    Add nil check to get_all_commits method

--- lib/tasks/commit_scraper.rb
+++ lib/tasks/commit_scraper.rb
@@ -1,7 +1,7 @@
 require 'open-uri'
 module GitScraper
 
-  def get_all_pushes(username)
+  def self.get_all_pushes(username)
     all_pushes = []
     events = JSON.parse(open("https://api.github.com/users/#{username}/events").read)
     events.each do |event|
@@ -12,25 +12,27 @@ module GitScraper
     all_pushes
   end
 
-  def get_all_commits(username)
+  def self.get_all_commits(username)
+    commits = []
     get_all_pushes(username).each do |push|
-      push['payload']['commits'].each do |commit|
-        commits << JSON.parse(open(commit['url']).read)
+      if push['payload']['commits'] != nil 
+        push['payload']['commits'].each do |commit|
+          commits << JSON.parse(open(commit['url']).read)
+        end
       end
     end
     commits
   end
 
-  def get_latest_push(username)
+  def self.get_latest_push(username)
     get_all_pushes(username).first
   end
 
-  def most_recent_commit(username)
+  def self.most_recent_commit(username)
     recent_commits = []
     get_latest_push(username)['payload']['commits'].each do |commit|
       recent_commits << JSON.parse(open(commit['url']).read)
     end
     recent_commits
   end
-
 end
