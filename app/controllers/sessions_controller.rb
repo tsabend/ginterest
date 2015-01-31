9591210d02bc537f6b8393eccc59aa46c4c88813
Thomas Abend <tsabend@gmail.com>
2015-01-31 11:30:03 -0500

    Differntiate signup from login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,14 @@ class SessionsController < ApplicationController
 
   def create
     auth = env['omniauth.auth']
-    user = User.find_or_create_by(email: auth["info"]["email"])
-    session[:user_id]
-    redirect_to user_path(user)
+    if user = User.where(email: auth["info"]["email"]).first
+    session[:user_id] = user.id
+    redirect_to user_path(User.find(session[:user_id]))
+    else
+      user = User.create_with_omniauth(auth)
+      session[:user_id] = user.id
+      redirect_to user_path(User.find(session[:user_id]))
+    end
   end
 
   def destroy
