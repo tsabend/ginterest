9b44ac83e6932075fc8bd8c91ff40e5359af1c0f
Thomas Abend <tsabend@gmail.com>
2015-01-31 12:43:24 -0500

    Github OAuth now redirects to root with notice

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,17 @@ class SessionsController < ApplicationController
     auth = env['omniauth.auth']
     if user = User.where(email: auth["info"]["email"]).first
     session[:user_id] = user.id
-    redirect_to user_path(User.find(session[:user_id]))
+    redirect_to root_url, :notice => "Welcome back #{user.username}"
     else
       user = User.create_with_omniauth(auth)
       session[:user_id] = user.id
-      redirect_to user_path(User.find(session[:user_id]))
+      redirect_to root_url, :notice => "Welcome #{user.username}"
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => ""
+    redirect_to root_url, :notice => "Peace"
   end
 
 end
