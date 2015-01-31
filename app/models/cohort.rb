5d65381c31f98c090f362690ecb3972c5ebfe53e
Thomas Abend <tsabend@gmail.com>
2015-01-31 10:23:44 -0500

    Add sessions controller

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,2 +1,4 @@
 class Cohort < ActiveRecord::Base
+  has_many :users
+  has_many :commits, through: :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  has_many :commits
+  belongs_to :cohort
 end
