e63d71d72c0a50912c5a488a08f2b6eb943e70d9
Thomas Abend <tsabend@gmail.com>
2015-01-31 11:24:27 -0500

    Add omniauth-github boilerplate

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,15 @@
 class SessionsController < ApplicationController
+
+  def create
+    auth = env['omniauth.auth']
+    user = User.find_or_create_by(email: auth["info"]["email"])
+    session[:user_id]
+    redirect_to user_path(user)
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, :notice => ""
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
   has_many :commits
   belongs_to :cohort
+
+  def self.create_with_omniauth(auth)
+    create do |user|
+      user.github_uid = auth["uid"]
+      user.name = auth["info"]["name"]
+      user.email = auth["info"]["email"]
+      user.username = auth["info"]["nickname"]
+      user.avatar_url = auth["info"]["image"]
+    end
+  end
 end
